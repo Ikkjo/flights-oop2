a85d5d6ae49d20940a08c977f9d835e7d79ea56e
ikkjo <ilija.kalinic@gmail.com>
2021-01-05 12:14:19 +0100

    Create IOException.h

---
+++ VS15/SortingFlights/IOException.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+
+class InvalidInputException : private std::exception {};
+// @TODO: Declare and define constructors
+
+class InvalidOutputException : private std::exception {};
+// @TODO: Declare and define constructors
